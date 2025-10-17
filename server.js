ef0b3bd8a26ee02974c423f66abcbcecf3a61c69
Joseph Allen <jallen17@tulane.edu>
2025-10-17 15:42:46 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
